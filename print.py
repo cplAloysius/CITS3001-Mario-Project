ccfa0baaff80ed3d40be6c17cf34337934e93f3d
livM07 <132204395+livM07@users.noreply.github.com>
2023-10-05 23:06:00 +0800

    mpPPO2 working!

--- print.py
+++ print.py
@@ -45,3 +45,30 @@ print(labml.__file__)
 print(labml.__package__)
 
 # /Users/oliviamorrison/anaconda3/envs/mario/lib/python3.8/site-packages/labml/__init__.py
+
+
+'''
+def _calc_advantages(self, done: np.ndarray, rewards: np.ndarray, values: np.ndarray) -> np.ndarray:
+        # Ensure the input numpy arrays are converted to PyTorch tensors and moved to the correct device.
+        rewards_t = torch.tensor(rewards[..., None], dtype=torch.float32, device=device)
+        done_t = torch.tensor(done[..., None], dtype=torch.float32, device=device)
+        values_t = torch.tensor(values[..., None], dtype=torch.float32, device=device)
+
+        # Assume that the last observed state is the next state after the last state in the trajectory.
+        _, last_value_t = self.model(obs_to_torch(self.obs))
+        last_value_t = last_value_t.squeeze(-1)[..., None, None]
+
+        # Using generalized_advantage_estimate function
+        next_state_values_t = torch.cat([values_t[..., 1:], last_value_t], dim=-2) #RuntimeError: Tensors must have same number of dimensions: got 3 and 2
+        advantages, _ = generalized_advantage_estimate(
+            gamma=self.gamma,
+            lmbda=self.lamda,
+            state_value=values_t,
+            next_state_value=next_state_values_t,
+            reward=rewards_t,
+            done=done_t
+        )
+
+        # Convert advantages back to numpy 
+        return advantages.squeeze(-1).cpu().numpy()
+'''
